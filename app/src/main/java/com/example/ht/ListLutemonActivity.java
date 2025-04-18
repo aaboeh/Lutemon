792226d5bc9c1f62e9b83c69a89e8da32f09b1aa
aaboeh <aapo.aarvala@gmail.com>
2025-04-18 19:06:14 +0300

    Great App

--- app/src/main/java/com/example/ht/ListLutemonActivity.java
+++ app/src/main/java/com/example/ht/ListLutemonActivity.java
@@ -1,6 +1,7 @@
 package com.example.ht;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,15 +11,22 @@ public class ListLutemonActivity extends AppCompatActivity {
 
     private Storage storage;
     private RecyclerView recyclerView;
+    private TextView listInfoText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_lutemon);
 
+        listInfoText = findViewById(R.id.ListInfoText);
+
         storage = Storage.getInstance();
 
         recyclerView = findViewById(R.id.ListLutemonRV);
+
+        if (storage.getLutemons().isEmpty()) {
+            listInfoText.setText("Lisää uusia lutemoneja, jotta niitä voidaan listata!");
+        }
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new LutemonListAdapter(getApplicationContext(), storage.getLutemons()));
